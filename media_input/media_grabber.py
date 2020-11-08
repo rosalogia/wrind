4f10615b357406881dc4c44335c7bc484f8b6bed
Ambika Eshwar <ambika.eshwar@rutgers.edu>
2020-11-08 02:01:04 -0500

    Implemented wikipedia and subtitle file media input

---
+++ media_input/media_grabber.py
@@ -0,0 +1,35 @@
+from urllib.parse import urlparse, unquote
+import wikipedia
+from pysubparser import parser as psparser
+from .set_generator import generate_sets
+
+def wikipedia_parser(parsed_url, splits=1, set_directory="."):
+    lang = parsed_url.netloc.split(".")[0]
+    title = parsed_url.path.split("/")
+
+    wikipedia.set_lang(lang)
+    page = wikipedia.page(title=title)
+    
+    generate_sets(page.content, splits, set_directory)
+
+def subtitles_parser(subtitle_file, splits=1, set_directory="."):
+    subs = psparser.parse(subtitle_file)
+
+    try:
+        lines = [line.text for line in subs]
+    except UnicodeDecodeError:
+        subs = psparser.parse(subtitle_file, encoding="ISO-8859-1")
+        lines = [line.text for line in subs]
+
+    text = " ".join(lines)
+    generate_sets(text, splits, set_directory)
+
+def router(inData, url=True, splits=1, set_directory="."):
+    if url:
+        parsed_url = urlparse(unquote(url))
+        if "wikipedia" in parsed_url.netloc:
+            wikipedia_parser(parsed_url, splits, set_directory)
+    elif inData.endswith((".srt", ".ass", ".ssa", ".sub")):
+        subtitles_parser(inData, splits, set_directory)
+    else:
+        generate_sets(text, splits, set_directory)
---
+++ media_input/set_generator.py
@@ -0,0 +1,61 @@
+import re
+from google.cloud import translate_v2 as translate
+import csv
+from html import unescape
+
+cleanse = lambda text: re.sub(r'[\W\d_]', '', text)
+
+# This line will throw an error if you have not set up your
+# gcloud credentials. Refer to the README for more information.
+translate_client = translate.Client()
+
+def commonality_table(text):
+    detected_language = translate_client.detect_language(text)['language']
+    print(detected_language)
+    if detected_language == 'zh-TW':
+        words = list(map(cleanse, list(text)))
+    else:
+        words = list(map(cleanse, text.lower().split()))
+    unique_words = set(words)
+    table = dict(zip(unique_words, [0] * len(unique_words)))
+
+    for word in words:
+        table[word] += 1
+
+    return table
+
+def sort_by_commonality(text):
+    ct = commonality_table(text)
+    return list(word for word, commonality in sorted(ct.items(), key=lambda i: i[1])[::-1])
+
+def translation_table(text):
+    detected_language = translate_client.detect_language(text)['language']
+    word_set = sort_by_commonality(text)
+    translated_words = [unescape(translate_client.translate(word, source_language=detected_language, target_language="en")['translatedText']) for word in word_set]
+    return list(zip(word_set, translated_words))
+
+def chunks(l, n):
+    split_size = len(l) // n
+    rem = len(l) % n
+    inner_max = len(l[:-rem]) if rem != 0 else len(l)
+
+    splits = [l[i:i+split_size] for i in range(0, inner_max, split_size)]
+
+    if rem != 0:
+        splits[-1].extend(l[-rem:])
+    return splits
+
+
+def generate_sets(text, splits=1, set_directory="."):
+    translations = translation_table(text)
+    size_per_set = len(translations) // splits
+    remaining = len(translations) % splits
+
+    split_sets = chunks(translations, splits)
+
+    for i in range(splits):
+        with open(f"{set_directory}/set_{i+1}.csv", "w", newline="") as set_file:
+            set_writer = csv.writer(set_file)
+            for (src, tgt) in split_sets[i]:
+                if src != tgt:
+                    set_writer.writerow([src, tgt])
