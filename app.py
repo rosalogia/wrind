82595ced311df8f0dbeac29491823e5ab3c183da
PAndaContron <rajtel99@gmail.com>
2020-11-08 11:01:54 -0500

    Disabled caching

--- app.py
+++ app.py
@@ -96,5 +96,14 @@ def advancedSubmit():
     return redirect(url_for("index"))
 
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers["Cache-Control"] = "public, max-age=0"
+    return r
+
+
 if __name__ == "__main__":
     app.run(debug=True)
