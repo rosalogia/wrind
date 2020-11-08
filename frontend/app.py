c7d99d642350b782029a6dbe0e1f86f9c71068fc
cakoshakib <cakoshakib@gmail.com>
2020-11-07 22:58:48 -0500

    Created frontend and implemented basic language decks

---
+++ frontend/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, request, send_file, redirect
+import csv
+import os.path
+from os import path
+
+app = Flask(__name__)
+text = "empty"
+text_area = False
+
+@app.route('/')
+def index():
+    return render_template('index.html', dl_visible="none", err_visible="none")
+
+@app.route("/", methods=["POST"])
+def submit():
+    global text
+    text = request.form['text']
+    if path.exists('language_decks/' + text + '.csv'):
+        return render_template('index.html', dl_visible="inline-block", err_visible="none") 
+    return render_template('index.html', dl_visible="none", err_visible="block") 
+
+@app.route("/getCSV")    
+def getCSV():
+    global text
+    text = request.form['text']
+    try:
+        filename = text.lower() + ".csv"
+        return send_file('language_decks/' + filename, mimetype='text/csv', attachment_filename=filename, as_attachment=True)
+    except:
+        return redirect(url_for('index'))
+
+
+@app.route("/adv")
+def convText():
+    global text_area
+    if text_area:
+        text_area = False
+        return render_template('index.html', dl_visible="none", err_visible="none", text_area=False) 
+    else:
+        text_area = True
+        return render_template('index.html', dl_visible="none", err_visible="none", text_area=True)  
+
+if __name__ == "__main__":
+    app.run(debug=True)
